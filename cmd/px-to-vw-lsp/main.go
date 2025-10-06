b5231f170961773eec8849dd1e166f61664412ea
meow_d <meow_d@tuta.io>
2025-10-06 10:16:50 +0800

    feat: arguments for log level and location

--- cmd/px-to-vw-lsp/main.go
+++ cmd/px-to-vw-lsp/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-  "go.uber.org/zap"
+	"flag"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func main() {
-	// TODO
+func parseFlags() (logLevel, logFile string) {
+	level := flag.String("log-level", "warn", "log level (debug, info, warn, error)")
+	file := flag.String("log-file", "/tmp/px-to-vw-lsp.log", "log file path")
+	flag.Parse()
+	return *level, *file
+}
+
+func initLogger(logLevel, logFile string) *zap.Logger {
 	cfg := zap.NewDevelopmentConfig()
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	cfg.OutputPaths = []string{ "stdout", "/tmp/px-to-vw-lsp.log" }
-  logger, _ := zap.NewDevelopment()
+	cfg.OutputPaths = []string{"stdout", logFile}
 
-  StartServer(logger)
+	level := zapcore.WarnLevel
+	switch logLevel {
+	case "debug":
+		level = zapcore.DebugLevel
+	case "info":
+		level = zapcore.InfoLevel
+	case "warn":
+		level = zapcore.WarnLevel
+	case "error":
+		level = zapcore.ErrorLevel
+	}
+	cfg.Level = zap.NewAtomicLevelAt(level)
+
+	logger, _ := cfg.Build()
+	return logger
+}
+
+func main() {
+	logLevel, logFile := parseFlags()
+	logger := initLogger(logLevel, logFile)
+	defer logger.Sync()
+
+	StartServer(logger)
 }
